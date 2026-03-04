4eeffd9b2a555ebb27020e4f33f8a6441611bc1b
LucasGoulart24 <Goulart7720@gmail.com>
2026-03-03 21:12:03 -0300

    filme

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom"
+import "../Css/Style.css"
 
 function Menu() {
     return (
-      <div className="w-full h-20 p-3 flex items-center justify-between bg-zinc-900 absolute">
+      <div className="w-full h-16 p-3 flex items-center justify-between bg-transparent backdrop-blur-2xl absolute">
         <h1 className="text-3xl text-emerald-500">
           <Link to="/">Good Movies</Link>
         </h1>
+        <div className="Busca w-96 h-10 flex items-center justify-center">
+          <input type="text" placeholder="Procurar filme" className="w-full h-full bg-gray-700 outline-0 p-4 text-white placeholder:text-white rounded-bl-2xl rounded-tl-2xl"/>
+          <button className="w-20 h-full bg-emerald-500 cursor-pointer active:bg-emerald-600 rounded-br-2xl rounded-tr-2xl transition-all duration-200">Search</button>
+        </div>
         <button className="w-25 p-2 rounded-2xl bg-emerald-500 text-white active:bg-emerald-600 transition-all duration-150">
           <Link to="/cadastro">Cadastro</Link>
         </button>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,8 @@
+import "../Css/Style.css"
+
 function Home() {
     return(
-        <div className="w-full h-screen flex items-center justify-center">
+        <div className="Home w-full h-screen flex items-center justify-center">
             <h1 className="text-4xl text-emerald-500">Hello</h1>
         </div>
     )
