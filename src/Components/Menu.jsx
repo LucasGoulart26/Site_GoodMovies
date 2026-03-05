1883bed974e7d7657a65784c3856349312fb3e15
LucasGoulart24 <Goulart7720@gmail.com>
2026-03-05 19:28:55 -0300

    filme

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -8,7 +8,7 @@ function Menu() {
           <Link to="/">Good Movies</Link>
         </h1>
         <div className="Busca w-96 h-10 flex items-center justify-center">
-          <input type="text" placeholder="Procurar filme" className="w-full h-full bg-gray-700 outline-0 p-4 text-white placeholder:text-white rounded-bl-2xl rounded-tl-2xl"/>
+          <input type="text" placeholder="Procurar filme" className="w-full h-full bg-gray-700 outline-0 p-4 text-white placeholder:text-white rounded-bl-2xl rounded-tl-2xl border-2 border-emerald-500"/>
           <button className="w-20 h-full bg-emerald-500 cursor-pointer active:bg-emerald-600 rounded-br-2xl rounded-tr-2xl transition-all duration-200">Search</button>
         </div>
         <button className="w-25 p-2 rounded-2xl bg-emerald-500 text-white active:bg-emerald-600 transition-all duration-150">
--- src/Pages/Cadastro.jsx
+++ src/Pages/Cadastro.jsx
@@ -15,8 +15,8 @@ function Cadastro() {
         setError(false)
     }
     return(
-        <div className="w-full h-screen flex items-center justify-center bg-zinc-800">
-            <div className="Cadastro w-[36%] p-4 rounded-2xl shadow-2xl bg-zinc-900 flex items-center justify-center flex-col">
+        <div className="Cadastro w-full h-screen flex items-center justify-center">
+            <div className="Card w-[36%] p-4 rounded-2xl shadow-2xl bg-zinc-900 flex items-center justify-center flex-col">
                 <img src="" alt="Perfil" className="w-32 h-32 rounded-[50%] bg-gray-50 border-2 border-gray-200"/>
                 <h1 className="text-3xl text-emerald-500 mt-2">Sign In</h1>
                 <div className="w-full flex items-start justify-items-start flex-col mt-2">
@@ -25,7 +25,7 @@ function Cadastro() {
                         type="text" 
                         value={userName}
                         onChange={(e)=> setUserName(e.target.value)}
-                        className={`w-full h-11 p-2 outline-0 rounded-[5px] border-2 border-gray-700 bg-gray-500 ${error && userName === "" ? "border-red-500 transition-all duration-200" : "border-gray-500"}`}
+                        className={`w-full h-11 p-3 outline-0 rounded-[5px] border-2 border-gray-700 bg-gray-500 text-white ${error && userName === "" ? "border-red-500 transition-all duration-200" : "border-gray-500"}`}
                     />
                 </div>
                 <div className="w-full flex items-start justify-items-start flex-col mt-2">
@@ -34,7 +34,7 @@ function Cadastro() {
                         type="email" 
                         value={email}
                         onChange={(e)=> setEmail(e.target.value)}
-                        className={`w-full h-11 outline-0 rounded-[5px] border-2 border-gray-700 bg-gray-500 ${error && email === "" ? "border-red-500 transition-all duration-200" : "border-gray-500"}`}
+                        className={`w-full h-11 p-3 outline-0 rounded-[5px] border-2 border-gray-700 bg-gray-500 text-white ${error && email === "" ? "border-red-500 transition-all duration-200" : "border-gray-500"}`}
                     />
                 </div>
                 <div className="w-full flex items-start justify-items-start flex-col mt-2">
@@ -43,7 +43,7 @@ function Cadastro() {
                         type="password" 
                         value={password}
                         onChange={(e)=> setPassword(e.target.value)}
-                        className={`w-full h-11 outline-0 rounded-[5px] border-2 border-gray-700 bg-gray-500 ${error && password === "" ? "border-red-500 transition-all duration-200" : "border-gray-500"}`}
+                        className={`w-full h-11 p-3 outline-0 rounded-[5px] border-2 border-gray-700 bg-gray-500 text-white ${error && password === "" ? "border-red-500 transition-all duration-200" : "border-gray-500"}`}
                     />
                 </div>
                 <button onClick={Cadastrar} className="w-full h-11 mt-2 cursor-pointer rounded-[5px] bg-emerald-500 text-white active:bg-emerald-600 transition-all duration-200">Cadastre-se</button>
