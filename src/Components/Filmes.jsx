ca00070a8e333f0703d5d0202b72e9bf97adbe45
LucasGoulart24 <Goulart7720@gmail.com>
2026-04-10 20:39:03 -0300

    filme

--- src/Components/Filmes.jsx
+++ src/Components/Filmes.jsx
@@ -7,6 +7,11 @@ import capaFilme5 from "../Img/Capas/capa-filme5.jpg";
 import capaFilme6 from "../Img/Capas/capa-filme6.jpg";
 import capaFilme7 from "../Img/Capas/capa-filme7.jpeg";
 import capaFilme8 from "../Img/Capas/capa-filme8.jpg";
+import capaFilme9 from "../Img/Capas/capa-filme9.jpg";
+import capaFilme10 from "../Img/Capas/capa-filme10.jpg";
+import capaFilme11 from "../Img/Capas/capa-filme11.jpg";
+import capaFilme12 from "../Img/Capas/capa-filme12.jpg";
+import capaFilme13 from "../Img/Capas/capa-filme13.jpg";
 import "../Css/Style.css";
 
 function Filmes() {
@@ -34,7 +39,12 @@ function Filmes() {
         {id: 5, ano: "2024", genero: "Ação", capa: capaFilme5},
         {id: 6, ano: "2024", genero: "Terror", capa: capaFilme6},
         {id: 7, ano: "2022", genero: "Ficção cientifica/Ação", capa: capaFilme7},
-        {id: 8, ano: "2026", genero: "Terror", capa: capaFilme8}
+        {id: 8, ano: "2026", genero: "Terror", capa: capaFilme8},
+        {id: 9, ano: "2025", genero: "Aventura/Comédia", capa: capaFilme9},
+        {id: 10, ano: "2024", genero: "Aventura/Comédia", capa: capaFilme10},
+        {id: 11, ano: "2020", genero: "Ação/Fantasia", capa: capaFilme11},
+        {id: 12, ano: "2009", genero: "Drama", capa: capaFilme12},
+        {id: 13, ano: "2022", genero: "Ação/Crime", capa: capaFilme13}
     ]
     const filmesFiltrados = active === "All" 
         ? filmes 
@@ -56,17 +66,17 @@ function Filmes() {
                 <button 
                     key={btn} 
                     onClick={()=> setActive(btn)} 
-                    className={`w-24 p-3 rounded-2xl cursor-pointer text-white ${active === btn ? "bg-emerald-500" : "bg-zinc-800"}`}
+                    className={`w-24 p-3 rounded-2xl cursor-pointer text-black ${active === btn ? "bg-emerald-500" : "bg-zinc-800 text-white"}`}
                 >{btn}</button>
             ))}
         </div>
-        <div className="w-full p-5 flex items-start justify-items-start flex-wrap gap-4">
+        <div className="w-full p-5 flex items-center justify-center flex-wrap gap-4">
             <div className="w-full p-2 border-b-2 border-b-emerald-500">
                 <h1 className="text-3xl text-emerald-500">Filmes</h1>
             </div>
             {filmesFiltrados.map((filme) => (
                 <div key={filme.id} className="CardFilme w-[24%] rounded-2xl bg-zinc-950 hover:shadow-2xl shadow-emerald-500 transition-all duration-300 ">
-                    <img src={filme.capa} alt="Capa" className="w-full h-96 bg-zinc-300"/>
+                    <img src={filme.capa} alt="Capa" className="w-full h-96"/>
                     <div className="p-3">
                         <div className="w-full flex items-center justify-between">
                             <p className="text-white">{filme.ano}</p>
