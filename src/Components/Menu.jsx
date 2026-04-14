cdfcd9434159ffe1ef9f911d58e4e5669c1a05a6
LucasGoulart24 <Goulart7720@gmail.com>
2026-04-14 20:52:12 -0300

    filme

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -17,7 +17,7 @@ function Menu() {
           <input
             type="text"
             placeholder="Procurar filme"
-            className="w-full h-full bg-gray-700 outline-0 p-4 text-white placeholder:text-white rounded-bl-2xl rounded-tl-2xl border-2 border-emerald-500"
+            className="w-full h-full bg-gray-800 outline-0 p-4 text-white placeholder:text-white rounded-bl-2xl rounded-tl-2xl border-2 border-emerald-500"
           />
           <button className="w-13 h-full flex items-center justify-center bg-emerald-500 cursor-pointer active:bg-emerald-600 rounded-br-2xl rounded-tr-2xl transition-all duration-200">
             <IoSearch className="text-3xl" />
--- src/Components/NovosFilmes.jsx
+++ src/Components/NovosFilmes.jsx
@@ -1,24 +1,39 @@
 import novoFilme from "../Img/capa-novofilme.jpeg";
 import "../Css/Style.css";
+import { useState } from "react";
 
 function NovosFilmes() {
+    const[index, setIndex] = useState(0);
+    
+    // Carrossel
+    const next = () => {
+      if(index < novosFilme.length - 1) {
+        setIndex(index + 1);
+      }
+    }
+    const prev = () => {
+      if(index > 0) {
+        setIndex(index - 1)
+      }
+    }
     const novosFilme = [
       {id: 1, capa: novoFilme, titulo: "Avatar: Fogo e Cinzas", data: "18/12/2025"},
       {id: 2, capa: novoFilme, titulo: "Avatar: Fogo e Cinzas", data: "18/12/2025"},
       {id: 3, capa: novoFilme, titulo: "Avatar: Fogo e Cinzas", data: "18/12/2025"},
       {id: 4, capa: novoFilme, titulo: "Avatar: Fogo e Cinzas", data: "18/12/2025"},
       {id: 5, capa: novoFilme, titulo: "Avatar: Fogo e Cinzas", data: "18/12/2025"},
+      {id: 6, capa: novoFilme, titulo: "Avatar: Fogo e Cinzas", data: "18/12/2025"}
     ];
     return (
       <div className="w-full h-screen flex items-center justify-center flex-col bg-cover bg-[url('/src/Img/backgroundcinematic.jpg')]">
-        <div className="w-96 border-b-2 border-emerald-500 p-2">
-          <h1 className="text-3xl text-white text-center">Novos Filmes</h1>
+        <div className="w-[70%] border-b-2 border-emerald-500 p-2">
+          <h1 className="text-3xl text-white">Novos Filmes</h1>
         </div>
-        <div className="w-full flex items-center justify-center mt-5">
-          <div className="w-full flex items-center justify-center flex-wrap gap-2">
+        <div className="relative w-[70%] overflow-hidden mt-5">
+          <div className="flex transition-transform duration-500 gap-2" style={{ transform: `translateX(-${index * 100}%)`}}>
             {novosFilme.map((filmes) => (
-              <div key={filmes.id} className="Card2 w-60 flex items-start justify-items-start flex-col rounded-2xl cursor-pointer bg-zinc-950">
-                <img src={filmes.capa} alt="Capa" className="w-full h-50 bg-gray-600"/>
+              <div key={filmes.id} className="Card2 w-1/4 shrink-0 flex items-start justify-items-start flex-col rounded-2xl cursor-pointer bg-zinc-950">
+                <img src={filmes.capa} alt="Capa" className="w-full h-80 bg-gray-600"/>
                 <div className="p-3">
                   <h1 className=" text-white">{filmes.titulo}</h1>
                   <h2 className="text-white">Lançamento: {filmes.data}</h2>
@@ -26,6 +41,8 @@ function NovosFilmes() {
               </div>
             ))}
           </div>
+          <button onClick={prev} className="absolute left-0 top-1/2 -translate-y-1/2 bg-white px-2 py-1 cursor-pointer">◀</button>
+          <button onClick={next} className="absolute right-0 top-1/2 -translate-y-1/2 bg-white px-2 py-1 cursor-pointer">▶</button>
         </div>
       </div>
     );
